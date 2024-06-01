c4e0df4335db4cffbb13ab278e66a1458c232174
Sai Kota <96785985+saikota7@users.noreply.github.com>
2024-06-01 19:00:24 +0530

    Delete backend directory

--- backend/models/Task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TaskSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    status: { type: String, default: 'to-do' }, // 'to-do', 'in-progress', 'done'
-});
-
-module.exports = mongoose.model('Task', TaskSchema);
